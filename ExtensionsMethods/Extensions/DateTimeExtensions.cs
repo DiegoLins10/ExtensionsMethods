399aa1b5b791b22188e9635c050c3eed0cd4db6e
DiegoLins10 <diegofernandeslins@gmail.com>
2021-11-28 20:17:55 -0300

    Add project files.

---
+++ ExtensionsMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExtensionsMethods.Extensions
+{
+    static class DateTimeExtensions
+    {
+        /*
+         * Um metodo que subtrai a data de agora 
+         * pela data passada como parametro no atributo DateTime
+         */
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if(duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ ExtensionsMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using ExtensionsMethods.Extensions;
+using System;
+
+namespace ExtensionsMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+            DateTime dt2 = new DateTime(2021, 11, 28, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+            Console.WriteLine(dt2.ElapsedTime());
+            TimeSpan duration = DateTime.Now.TimeOfDay;
+            Console.WriteLine(duration);
+        }
+    }
+}
